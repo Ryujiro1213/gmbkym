e9e61fb6e1e67c95965da38cb4afdc66b6bf6193
Ryujiro1213 <92771795+Ryujiro1213@users.noreply.github.com>
2021-10-19 14:54:35 +0900

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -3,16 +3,20 @@ import signal
 import sys
 import time
 import picamera
+import cv2
+import os
 from time import sleep
 
 GPIO.setmode (GPIO.BCM)
 GPIO.setup (17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-  sw_status = GPIO.input (17)
-  if sw_status == 0:
-    with picamera.PiCamera() as camera:
-      camera.resolution = (1024, 768)
+  #sw_status = GPIO.input (17)
+  #if sw_status == 0:
+  a=input()
+  with picamera.PiCamera() as camera:
+     camera.resolution = (1024, 768)
+  if a==1:
       camera.start_preview()
       time.sleep(2)
       camera.capture('my_pic.jpg')
